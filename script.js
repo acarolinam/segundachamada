29e71648bc6e3fcdf086b903492a5c8159b2d8bd
Ana Carolina <acarolinamat@gmail.com>
2018-04-13 15:15:54 -0300

    Enviando templates do projeto

---
+++ script.js
@@ -0,0 +1,23 @@
+function habilitarAluno(){
+  if (document.getElementById("login-aluno").style.display =='none'){
+    document.getElementById("login-aluno").style.display='inline';
+  } else {
+    document.getElementById("login-aluno").style.display='none';
+  }
+}
+
+function habilitarProfessor(){
+  if (document.getElementById("login-professor").style.display =='none'){
+    document.getElementById("login-professor").style.display='inline';
+  } else {
+    document.getElementById("login-professor").style.display='none';
+  }
+}
+
+function habilitarCoordenador(){
+  if (document.getElementById("login-coordenador").style.display =='none'){
+    document.getElementById("login-coordenador").style.display='inline';
+  } else {
+    document.getElementById("login-coordenador").style.display='none';
+  }
+}
